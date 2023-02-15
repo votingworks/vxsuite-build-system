eb92ced0f8fc29db97b5225e1801e74691ddcca1
Adam McManus <adam.mcmanus@gmail.com>
2023-02-15 15:14:04 -0600

    custom circle image checkpoint

---
+++ scripts/install-base-packages.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+DEBIAN_FRONTEND=noninteractive apt update
+DEBIAN_FRONTEND=noninteractive apt install -y sudo git make build-essential curl wget ssh tar gzip ca-certificates libx11-dev libpng-dev libjpeg-dev
+
+exit 0
