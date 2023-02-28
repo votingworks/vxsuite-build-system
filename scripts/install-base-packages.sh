481f9eaaa42cf86489ffd21ab1c5eb0117d8da5c
Adam McManus <adam.mcmanus@gmail.com>
2023-02-28 16:30:42 -0600

    add sudo for base package install

--- scripts/install-base-packages.sh
+++ scripts/install-base-packages.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-DEBIAN_FRONTEND=noninteractive apt update
-DEBIAN_FRONTEND=noninteractive apt install -y sudo git make build-essential curl wget ssh tar gzip ca-certificates libx11-dev libpng-dev libjpeg-dev xvfb zip
+DEBIAN_FRONTEND=noninteractive sudo apt update
+DEBIAN_FRONTEND=noninteractive sudo apt install -y sudo git make build-essential curl wget ssh tar gzip ca-certificates libx11-dev libpng-dev libjpeg-dev xvfb zip
 
 exit 0
