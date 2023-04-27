56bd04147a0a020a795b0de939528abb3cbc8cea
Adam McManus <adam.mcmanus@gmail.com>
2023-04-27 10:43:21 -0500

    misc config playbooks (#31)
    Various service configs like cups, speech synth, vxmark.

---
+++ playbooks/files/setup-speech-synthesis.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# save current directory
+INSTALL_DIR=$(pwd)
+
+mkdir -p /tmp/vx
+cd /tmp/vx
+
+# Festival and Speech Dispatcher
+sudo apt install -y festival speech-dispatcher speech-dispatcher-festival festvox-us-slt-hts
+wget http://festvox.org/cmu_arctic/cmu_arctic/packed/cmu_us_slt_arctic-0.95-release.tar.bz2
+bunzip2 cmu_us_slt_arctic-0.95-release.tar.bz2
+tar xf cmu_us_slt_arctic-0.95-release.tar
+
+if [[ $(lsb_release -cs) == "bionic" ]]; then
+  sudo mkdir -p /usr/share/festival/voices/english/
+  sudo mv cmu_us_slt_arctic /usr/share/festival/voices/english/cmu_us_slt_arctic_clunits
+else
+  sudo mkdir -p /usr/share/festival/voices/us/
+	sudo mv cmu_us_slt_arctic /usr/share/festival/voices/us/cmu_us_slt_arctic_clunits
+fi
+  
+# done with downloaded files
+cd "$INSTALL_DIR"
+rm -rf /tmp/vx
+
+# set up festival voice
+sudo cp ./files/speechd.conf /etc/speech-dispatcher/
+
+# festival auto-start
+sudo cp ./files/vx-festival.service /etc/systemd/system/
+sudo systemctl enable vx-festival
+sudo systemctl start vx-festival
