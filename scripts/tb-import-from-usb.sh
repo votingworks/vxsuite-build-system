827c0e9de7803b9444e696c1e5d983a2a9a2aebe
Adam McManus <adam.mcmanus@gmail.com>
2023-07-06 13:51:51 -0500

    workaround bash not expanding the home directory via tilde

--- scripts/tb-import-from-usb.sh
+++ scripts/tb-import-from-usb.sh
@@ -3,6 +3,7 @@
 #-- Gonna run as sudo, so we need the local user 
 local_user=`logname`
 usb_root="/media/${local_user}/VxTrustedBuild"
+local_user_home_dir=$( getent passwd "${local_user}" | cut -d: -f6 )
 
 #-- Make sure the USB is mounted where we expect
 if [ ! -d $usb_root ]; then
@@ -15,5 +16,9 @@ cp -r ${usb_root}/downloads /tmp/
 
 #-- Copy all the apt packages to the local cache
 cp -r ${usb_root}/apt_packages/* /var/cache/apt/archives/
+
+#-- Copy vxsuite-complete-system
+cp -r ${usb_root}/vxsuite-complete-system ${local_user_home_dir}/code/
+chown -R ${local_user}.${local_user} ${local_user_home_dir}/code/vxsuite-complete-system
 
 exit 0;
