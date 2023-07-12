edac728e872f15bbf940b290b234851751311138
Adam McManus <adam.mcmanus@gmail.com>
2023-07-12 11:09:20 -0500

    add cargo and pnpm to import

--- scripts/tb-import-from-usb.sh
+++ scripts/tb-import-from-usb.sh
@@ -4,6 +4,8 @@
 local_user=`logname`
 usb_root="/media/${local_user}/VxTrustedBuild"
 local_user_home_dir=$( getent passwd "${local_user}" | cut -d: -f6 )
+cargo_dir="${local_user_home_dir}/.cargo/registry/"
+pnpm_dir="${local_user_home_dir}/.local/share/pnpm/"
 
 #-- Make sure the USB is mounted where we expect
 if [ ! -d $usb_root ]; then
@@ -20,5 +22,21 @@ cp -r ${usb_root}/apt_packages/* /var/cache/apt/archives/
 #-- Copy vxsuite-complete-system
 cp -r ${usb_root}/vxsuite-complete-system ${local_user_home_dir}/code/
 chown -R ${local_user}.${local_user} ${local_user_home_dir}/code/vxsuite-complete-system
+
+#-- Copy vxsuite-build-system
+cp -r ${usb_root}/vxsuite-build-system ${local_user_home_dir}/code/
+chown -R ${local_user}.${local_user} ${local_user_home_dir}/code/vxsuite-build-system
+
+#-- Copy cargo packages
+if [ ! -d $cargo_dir ]; then
+  mkdir -p $cargo_dir
+fi
+cp -r ${usb_root}/cargo_packages/* $cargo_dir
+
+#-- Copy pnpm packages
+if [ ! -d $pnpm_dir ]; then
+  mkdir -p $pnpm_dir
+fi
+cp -r ${usb_root}/pnpm_packages/* $pnpm_dir
 
 exit 0;
