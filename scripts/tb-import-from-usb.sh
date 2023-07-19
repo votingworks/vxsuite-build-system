1eecd4bcdea8892ada43c366c6f1403f9a6302f7
Adam McManus <adam.mcmanus@gmail.com>
2023-07-19 11:17:51 -0500

    Adam/tb inventory (#47)
    * inventory for tb testing
    
    * add cargo and pnpm downloads to well known paths
    
    * add cargo and pnpm to import
    
    * usb export performance and import permissions
    
    * remove trailing backslash from copy
    
    * dir perms hack for now
    
    * ensure code dir exists and has correct ownership
    
    * kiosk browser apt packages
    
    * one of those days
    
    * add more base packages
    
    * initial rubygems support
    
    * command uses different chdir structure
    
    * update cmd line arg
    
    * typo
    
    * fix path
    
    * switch to shell
    
    * ensure temp gems directory exists
    
    * add electron support
    
    * fix electron cache path
    
    * yarn cache
    
    * add electron-gyp

--- scripts/tb-import-from-usb.sh
+++ scripts/tb-import-from-usb.sh
@@ -4,6 +4,12 @@
 local_user=`logname`
 usb_root="/media/${local_user}/VxTrustedBuild"
 local_user_home_dir=$( getent passwd "${local_user}" | cut -d: -f6 )
+code_dir="${local_user_home_dir}/code"
+cargo_dir="${local_user_home_dir}/.cargo/registry/"
+pnpm_dir="${local_user_home_dir}/.local/share/pnpm/"
+electron_dir="${local_user_home_dir}/.cache/electron/"
+electron_gyp_dir="${local_user_home_dir}/.electron-gyp/"
+yarn_dir="${local_user_home_dir}/.cache/yarn/"
 
 #-- Make sure the USB is mounted where we expect
 if [ ! -d $usb_root ]; then
@@ -17,8 +23,56 @@ cp -r ${usb_root}/downloads /tmp/
 #-- Copy all the apt packages to the local cache
 cp -r ${usb_root}/apt_packages/* /var/cache/apt/archives/
 
+if [ ! -d $code_dir ]; then
+  mkdir -p $code_dir
+  chown ${local_user}.${local_user} $code_dir
+fi
+
 #-- Copy vxsuite-complete-system
-cp -r ${usb_root}/vxsuite-complete-system ${local_user_home_dir}/code/
-chown -R ${local_user}.${local_user} ${local_user_home_dir}/code/vxsuite-complete-system
+cp -r ${usb_root}/vxsuite-complete-system $code_dir
+chown -R ${local_user}.${local_user} ${code_dir}/vxsuite-complete-system
+
+#-- Copy vxsuite-build-system
+cp -r ${usb_root}/vxsuite-build-system $code_dir
+chown -R ${local_user}.${local_user} ${code_dir}/vxsuite-build-system
+
+#-- Copy cargo packages
+if [ ! -d $cargo_dir ]; then
+  mkdir -p $cargo_dir
+  chown -R ${local_user}.${local_user} ${local_user_home_dir}/.cargo
+fi
+cp -r ${usb_root}/cargo_packages/* $cargo_dir
+chown -R ${local_user}.${local_user} $cargo_dir
+
+#-- Copy pnpm packages
+if [ ! -d $pnpm_dir ]; then
+  mkdir -p $pnpm_dir
+fi
+cp -r ${usb_root}/pnpm_packages/* $pnpm_dir
+chown -R ${local_user}.${local_user} $pnpm_dir
+
+#-- Copy electron cache
+if [ ! -d $electron_dir ]; then
+  mkdir -p $electron_dir
+  chown -R ${local_user}.${local_user} $electron_dir
+fi
+cp -r ${usb_root}/electron_cache/* $electron_dir
+chown -R ${local_user}.${local_user} $electron_dir
+
+#-- Copy electron-gyp cache
+if [ ! -d $electron_gyp_dir ]; then
+  mkdir -p $electron_gyp_dir
+  chown -R ${local_user}.${local_user} $electron_gyp_dir
+fi
+cp -r ${usb_root}/electron_gyp_cache/* $electron_gyp_dir
+chown -R ${local_user}.${local_user} $electron_gyp_dir
+
+#-- Copy yarn cache
+if [ ! -d $yarn_dir ]; then
+  mkdir -p $yarn_dir
+  chown -R ${local_user}.${local_user} $yarn_dir
+fi
+cp -r ${usb_root}/yarn_cache/* $yarn_dir
+chown -R ${local_user}.${local_user} $yarn_dir
 
 exit 0;
