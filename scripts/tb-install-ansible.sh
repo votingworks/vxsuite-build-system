96d26230bd9fe2afba799bc3b0ae5c5d161e25ff
Adam McManus <adam.mcmanus@gmail.com>
2023-08-24 13:32:45 -0500

    Convert pip to require checksums (#61)
    * use hashes for pip installs
    
    * path fix
    
    * ensure ansible install bails on any failure since --require-hashes is now used with pip
    
    * explicit versions for python packages

--- scripts/tb-install-ansible.sh
+++ scripts/tb-install-ansible.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 phase=$1
 
 if [ "$phase" != "online" ] && [ "$phase" != "offline" ] && [ "$phase" != "" ]; then
@@ -13,18 +15,21 @@ fi
 
 echo "The phase is $phase."
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 function apt_install ()
 {
   local phase=$1
+  local python_packages="python3.9=3.9.2-1 python3-pip=20.3.4-4+deb11u1"
   if [ "$phase" == "online" ] || [ "$phase" == "both" ]; then
     echo "online/both"
-    apt-get install --reinstall --download-only -y python3.9 python3-pip
-    apt-get install -y python3.9 python3-pip
+    apt-get install --reinstall --download-only -y ${python_packages}
+    apt-get install -y ${python_packages}
   fi
 
   if [ "$phase" == "offline" ] || [ "$phase" == "both" ]; then
     echo "offline/both"
-    apt-get install -y python3.9 python3-pip
+    apt-get install -y ${python_packages}
   fi
 }
 
@@ -33,13 +38,13 @@ function pip_install ()
   local phase=$1
   if [ "$phase" == "online" ] || [ "$phase" == "both" ]; then
     echo "online/both"
-    pip3 download -d /var/tmp/downloads ansible passlib pipenv 
-    pip3 install --no-index --find-links /var/tmp/downloads ansible passlib pipenv
+    pip3 download -d /var/tmp/downloads --require-hashes -r ${DIR}/pip_requirements.txt
+    pip3 install --no-index --find-links /var/tmp/downloads --require-hashes -r ${DIR}/pip_requirements.txt
   fi
 
   if [ "$phase" == "offline" ] || [ "$phase" == "both" ]; then
     echo "offline/both"
-    pip3 install --no-index --find-links /var/tmp/downloads ansible passlib pipenv
+    pip3 install --no-index --find-links /var/tmp/downloads --require-hashes -r ${DIR}/pip_requirements.txt
   fi
 }
 
