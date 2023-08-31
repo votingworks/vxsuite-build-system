c3d3a41717dab70d63c0307aa4515d13c1a84b72
Adam McManus <adam.mcmanus@gmail.com>
2023-08-31 12:12:05 -0500

    make inventory a command line arg

--- scripts/tb-run-online-phase.sh
+++ scripts/tb-run-online-phase.sh
@@ -8,6 +8,14 @@ local_user_home_dir=$( getent passwd "${local_user}" | cut -d: -f6 )
 vxsuite_build_system_dir="${local_user_home_dir}/code/vxsuite-build-system"
 vxsuite_complete_system_dir="${local_user_home_dir}/code/vxsuite-complete-system"
 
+ansible_inventory=$1
+
+if [[ ! -d ${vxsuite_build_system_dir}/inventories/${ansible_inventory} ]]; then
+  echo "ERROR: The $ansible_inventory inventory could not be found."
+  echo "You can find a list of inventories in: ${vxsuite_build_system_dir}/inventories"
+  exit 1
+fi
+
 if [[ ! -d $vxsuite_build_system_dir ]]; then
   echo "ERROR: vxsuite-build-system could not be found."
   exit 1
@@ -29,7 +37,7 @@ fi
 
 echo "Run prepare_for_build playbook. This will take several minutes."
 sleep 5
-ansible-playbook -i inventories/tb playbooks/trusted_build/prepare_for_build.yaml
+ansible-playbook -i inventories/${ansible_inventory} playbooks/trusted_build/prepare_for_build.yaml
 
 if [[ ! -d $vxsuite_complete_system_dir ]]; then
   echo "ERROR: vxsuite-complete-system could not be found."
@@ -44,9 +52,9 @@ cd $vxsuite_complete_system_dir
 echo "Download necessary tools for TPM."
 sleep 5
 cd $vxsuite_build_system_dir
-ansible-playbook -i inventories/tb playbooks/trusted_build/tpm.yaml --skip-tags offline
+ansible-playbook -i inventories/${ansible_inventory} playbooks/trusted_build/tpm.yaml --skip-tags offline
 
 echo "The online phase is complete. Please insert a USB drive and run: "
-echo "ansible-playbook -i inventories/tb playbooks/trusted_build/export_to_usb.yaml"
+echo "ansible-playbook -i inventories/${ansible_inventory} playbooks/trusted_build/export_to_usb.yaml"
 
 exit 0
