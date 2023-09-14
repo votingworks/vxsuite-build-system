e745da8f0dc5a10b87926a2e5c2535985c1d5fbf
Adam McManus <adam.mcmanus@gmail.com>
2023-09-14 13:04:42 -0500

    more scripts

--- scripts/tb-export-to-usb.sh
+++ scripts/tb-export-to-usb.sh
@@ -6,7 +6,6 @@ debian_major_version=$(cat /etc/debian_version | cut -d'.' -f1)
 local_user=`logname`
 local_user_home_dir=$( getent passwd "${local_user}" | cut -d: -f6 )
 vxsuite_build_system_dir="${local_user_home_dir}/code/vxsuite-build-system"
-vxsuite_complete_system_dir="${local_user_home_dir}/code/vxsuite-complete-system"
 
 ansible_inventory=$1
 
@@ -35,26 +34,8 @@ if [[ "$debian_major_version" == "12" ]]; then
   source .virtualenv/ansible/bin/activate
 fi
 
-echo "Run prepare_for_build playbook. This will take several minutes."
+echo "Export VotingWorks tools and repositories to USB"
 sleep 5
-ansible-playbook -i inventories/${ansible_inventory} playbooks/trusted_build/prepare_for_build.yaml
-
-if [[ ! -d $vxsuite_complete_system_dir ]]; then
-  echo "ERROR: vxsuite-complete-system could not be found."
-  exit 1
-fi
-
-echo "Run prepare_build.sh in complete-system. This will take several minutes."
-sleep 5
-cd $vxsuite_complete_system_dir
-./prepare_build.sh
-
-echo "Download necessary tools for TPM."
-sleep 5
-cd $vxsuite_build_system_dir
-ansible-playbook -i inventories/${ansible_inventory} playbooks/trusted_build/tpm.yaml --skip-tags offline
-
-echo "The online phase is complete. Please insert a USB drive and run: "
-echo "./scripts/tb-export-to-usb.sh ${ansible_inventory}"
+ansible-playbook -i inventories/${ansible_inventory} playbooks/trusted_build/export_to_usb.yaml
 
 exit 0
