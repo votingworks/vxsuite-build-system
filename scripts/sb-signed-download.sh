c9d884414ea0d981113629561b3347f6744ba372
Adam McManus <adam.mcmanus@gmail.com>
2024-01-09 11:52:52 -0600

    initial upload script for unsigned image

---
+++ scripts/sb-signed-download.sh
@@ -0,0 +1,5 @@
+# download all 4 from s3://votingworks-trusted-build/signed/
+# hash img.lz4
+# hash xml
+# hash vars
+# compare hash reference file
---
+++ scripts/sb-signed-upload.sh
@@ -0,0 +1,6 @@
+# dump xml
+# hash img.lz4
+# hash xml
+# hash vars
+# create hash reference file
+# upload all 4 to s3://votingworks-trusted-build/signed/
