bd8af60468b5d2940eca1516e609dd2a43fc6372
Adam McManus <adam.mcmanus@gmail.com>
2024-01-09 14:58:39 -0600

    initial sb-signed-download script

--- scripts/sb-signed-download.sh
+++ scripts/sb-signed-download.sh
@@ -6,26 +6,26 @@ vm_name=$1
 vm_img_path="/var/lib/libvirt/images/${vm_name}.img"
 vm_img_zip_path="${vm_img_path}.lz4"
 vm_vars_path="/var/lib/libvirt/qemu/nvram/${vm_name}_VARS.fd"
-vm_xml_path="/tmp/${vm_name}-unsigned.xml"
-hash_ref_path="/tmp/${vm_name}-unsigned-hashes.txt"
+vm_xml_path="/tmp/${vm_name}-signed.xml"
+hash_ref_path="/tmp/${vm_name}-signed-hashes.txt"
 verify_hash_ref_path="/tmp/${vm_name}-verification-hashes.txt"
-s3_path="s3://votingworks-trusted-build/unsigned/"
-s3_hash_ref_path="${s3_path}/${vm_name}-unsigned-hashes.txt"
+s3_path="s3://votingworks-trusted-build/signed"
+s3_hash_ref_path="${s3_path}/${vm_name}-signed-hashes.txt"
 s3_vars_path="${s3_path}/${vm_name}_VARS.fd"
-s3_xml_path="${s3_path}/${vm_name}-unsigned.xml"
+s3_xml_path="${s3_path}/${vm_name}-signed.xml"
 s3_img_zip_path="${s3_path}/${vm_name}.img.lz4"
 
 # Download all files from S3
 echo ""
-echo "Downloading unsigned files from ${s3_path}. This will take a few minutes."
+echo "Downloading signed files from ${s3_path}. This will take a few minutes."
 aws s3 cp $s3_hash_ref_path $hash_ref_path
 aws s3 cp $s3_vars_path $vm_vars_path
 aws s3 cp $s3_xml_path $vm_xml_path
 aws s3 cp $s3_img_zip_path $vm_img_zip_path
 
 # Verify the image exists
-if [[ ! -f $vm_img_path ]]; then
-  echo "ERROR: The $vm_name img ($vm_img_path) could not be found."
+if [[ ! -f $vm_img_zip_path ]]; then
+  echo "ERROR: The compressed $vm_name img ($vm_img_zip_path) could not be found."
   exit 1
 fi
 
@@ -38,13 +38,13 @@ fi
 
 # Generate sha256 hashes of all files
 echo ""
-echo "Calculating sha256 hashes of all unsigned files to verify against."
-sha256sum $vm_img_zip_path >> $hash_ref_path
-sha256sum $vm_vars_path >> $hash_ref_path
-sha256sum $vm_xml_path >> $hash_ref_path
+echo "Calculating sha256 hashes of all signed files to verify against."
+sha256sum $vm_img_zip_path >> $verify_hash_ref_path
+sha256sum $vm_vars_path >> $verify_hash_ref_path
+sha256sum $vm_xml_path >> $verify_hash_ref_path
 
 echo ""
-echo "Comparing hashes of all unsigned files."
+echo "Comparing hashes of all signed files."
 if ! diff $hash_ref_path $verify_hash_ref_path >/dev/null 2>&1
 then
   echo "ERROR: Hashes do not match."
@@ -53,17 +53,8 @@ else
   echo "Successfully verified all unsigned files hashes are correct."
 fi
 
-# Decompress the img w/ lz4
 echo ""
-echo "Decompressing ${vm_img_path}. This may take a few minutes."
-lz4 $vm_img_zip_path
-
-# Export the image's XML configuration
-echo "Creating the $vm_name configuration."
-virsh create $vm_xml_path
-
-echo ""
-echo "The unsigned download process is now complete."
+echo "The signed download process is now complete."
 echo ""
 
 exit 0;
