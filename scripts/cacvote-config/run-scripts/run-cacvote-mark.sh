8e2117cf9498d8f1ffd2488b6fc94ff33595f022
Adam McManus <adam.mcmanus@gmail.com>
2024-09-19 15:09:20 -0500

    initial attempt at copying relevant system config

---
+++ scripts/cacvote-config/run-scripts/run-cacvote-mark.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# go to directory where this file is located
+cd "$(dirname "$0")"
+
+# configuration information
+CONFIG=${VX_CONFIG_ROOT:-./config}
+METADATA=${VX_METADATA_ROOT:-./}
+#source ${CONFIG}/read-vx-machine-config.sh
+
+cd cacvote/apps/cacvote-mark/
+(trap 'kill 0' SIGINT SIGHUP; make -C backend run & cd frontend/prodserver && node index.js)
