184edba8aa1655c5c392a9ed572bb380b1f36598
Adam McManus <adam.mcmanus@gmail.com>
2024-09-23 15:12:15 -0500

    source env and configure workspace

--- scripts/cacvote-config/run-scripts/run-cacvote-mark.sh
+++ scripts/cacvote-config/run-scripts/run-cacvote-mark.sh
@@ -8,7 +8,8 @@ cd "$(dirname "$0")"
 # configuration information
 CONFIG=${VX_CONFIG_ROOT:-./config}
 METADATA=${VX_METADATA_ROOT:-./}
-#source ${CONFIG}/read-vx-machine-config.sh
+source ${CONFIG}/read-vx-machine-config.sh
+export CACVOTE_MARK_WORKSPACE="/vx/data/cacvote-mark-workspace"
 
 cd cacvote/apps/cacvote-mark/
 (trap 'kill 0' SIGINT SIGHUP; make -C backend run & cd frontend/prodserver && node index.js)
