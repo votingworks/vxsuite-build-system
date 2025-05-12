c939cd0cc93ead32097119dc444be5da4f2500be
Adam McManus <adam.mcmanus@gmail.com>
2025-05-12 09:24:22 -0500

    update vxpollbook to support new var expansion approach

--- scripts/setup-pollbook-machine.sh
+++ scripts/setup-pollbook-machine.sh
@@ -196,6 +196,12 @@ sudo ln -sf /var/vx/config /vx/config
 
 sudo ln -s /vx/code/config/read-vx-machine-config.sh /vx/config/read-vx-machine-config.sh
 
+# To support new images installed via older vx-iso releases
+# explicitly set the EXPAND_VAR flag file
+# (This gets set by new vx-iso releases, so we can eventually remove
+# setting the flag here, but it doesn't hurt to leave it)
+sudo touch /vx/config/EXPAND_VAR
+
 # machine manufacturer
 sudo sh -c 'echo "VotingWorks" > /vx/config/machine-manufacturer'
 
