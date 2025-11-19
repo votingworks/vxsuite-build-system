518be5adfe0bee291dacd6ea0f61cb88a42ed7f7
Adam McManus <adam.mcmanus@gmail.com>
2025-11-19 09:25:13 -0600

    Add usbguard support for blocking/allowing mass storage, keyboard, and mice USB devices (#196)
    * try it all via automation
    
    * usbguard during offline build phase
    
    * usbguard permissions are insane, use a different approach
    
    * ensure /var/etc exists

---
+++ playbooks/trusted_build/files/set-usb-port-status.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+
+set -euo pipefail
+
+MASS_STORAGE="08:*:*"
+KEYBOARD="03:01:*"
+MOUSE="03:02:*"
+
+rules_path="/var/etc/usbguard-rules.conf"
+
+# generate list of initial allowed devices
+usbguard generate-policy | grep -v ${MASS_STORAGE} | grep -v ${KEYBOARD} | grep -v ${MOUSE} > ${rules_path}
+
+if [[ $1 == "block" ]]; then
+  BLOCK_RULES=$(cat <<EOF
+# Block external USB storage, keyboards, and mice
+block with-interface one-of { ${MASS_STORAGE} }
+block with-interface one-of { ${KEYBOARD} }
+block with-interface one-of { ${MOUSE} }
+EOF
+)
+  echo "$BLOCK_RULES" >> ${rules_path}
+  systemctl restart usbguard
+elif [[ $1 == "allow" ]]; then
+  ALLOW_RULES=$(cat <<EOF
+# Allow external USB storage, keyboards, and mice
+allow with-interface one-of { ${MASS_STORAGE} }
+allow with-interface one-of { ${KEYBOARD} }
+allow with-interface one-of { ${MOUSE} }
+EOF
+)
+  echo "$ALLOW_RULES" >> ${rules_path}
+  systemctl restart usbguard
+else
+  if grep -q "allow with-interface" ${rules_path}; then
+    echo "usb allowed"
+  else
+    echo "usb blocked"
+  fi
+fi
+
+exit 0
