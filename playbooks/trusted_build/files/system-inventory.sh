54f8142b50414c479a9a61bcbacbdcf02a61495f
Adam McManus <adam.mcmanus@gmail.com>
2026-03-09 15:25:23 -0500

    additional error checking, removed serial number and firmware from some device types that don't support it, updated file permissions

--- playbooks/trusted_build/files/system-inventory.sh
+++ playbooks/trusted_build/files/system-inventory.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euo pipefail
+
 # Exit if not running as root
 [[ $EUID -ne 0 ]] && echo "Please run as root." && exit 1
 
@@ -15,7 +17,6 @@ DATA["sbc_bios_vendor"]=$(dmidecode -s bios-vendor 2>/dev/null || echo "NA")
 DATA["sbc_bios_version"]=$(dmidecode -s bios-version 2>/dev/null || echo "NA")
 DATA["sbc_bios_release_date"]=$(dmidecode -s bios-release-date 2>/dev/null || echo "NA")
 DATA["sbc_bios_revision"]=$(dmidecode -s bios-revision 2>/dev/null || echo "NA")
-DATA["sbc_serial_number"]=$(dmidecode -s system-serial-number 2>/dev/null || echo "NA")
 DATA["tpm_version"]=$(cat /sys/class/tpm/tpm0/tpm_version_major || echo "NA")
 
 # Disks
@@ -26,29 +27,29 @@ do
 done
 
 # Temperature
-nvme_adapter=$(sensors | grep -i nvme)
-DATA["temp_nvme_cpu"]=$(sensors -f -A ${nvme_adapter} | grep Composite | awk '{print $2}' || echo "NA")
+nvme_adapter=$(sensors | grep -i nvme) || nvme_adapter=""
+if [[ ! -z "${nvme_adapter}" ]]; then
+  DATA["temp_nvme_cpu"]=$(sensors -f -A ${nvme_adapter} | grep Composite | awk '{print $2}' || echo "NA")
+fi
 
 # Display
-# Serial Number is basically impossible to reliably detect, so not including it.
-DATA["screen_brightness"]=$(ddcutil getvcp 10 --brief 2>/dev/null | awk '{print $4}' || echo "NA")
-DATA["screen_firmware"]=$(ddcutil getvcp c9 2>/dev/null | cut -d':' -f2 | xargs || echo "NA")
+if ddcutil getvcp 10 --brief > /dev/null 2>&1; then
+  DATA["screen_brightness"]=$(ddcutil getvcp 10 --brief | awk '{print $4}' || echo "NA")
+fi
 
 # Card Reader
-# Serial Number is basically impossible to reliably detect, so not including it.
-vidpid=$(lsusb | grep -i "reader" | head -n 1 | awk '{print $6}')
-DATA["card_reader_info"]=$(lsusb | grep -i "reader" | head -n 1 || echo "NA")
-DATA["card_reader_version"]=$(lsusb -v -d ${vidpid} | grep bcdDevice | awk '{print $2}' || echo "NA")
+vidpid=$(lsusb | grep -i "reader" | head -n 1 | awk '{print $6}') || vidpid=""
+if [[ ! -z "${vidpid}" ]]; then
+  DATA["card_reader_info"]=$(lsusb | grep -i "reader" | head -n 1 || echo "NA")
+  DATA["card_reader_version"]=$(lsusb -v -d ${vidpid} | grep bcdDevice | awk '{print $2}' || echo "NA")
+fi
 
 # Scanner
-#
-scanner_match=$( sane-find-scanner -q | grep -m 1 "libusb" )
+scanner_match=$( sane-find-scanner -q | grep -m 1 "libusb" ) || scanner_match=""
 
 if [[ -z "$scanner_match" ]]; then
   DATA["scanner_vendor"]="NA"
   DATA["scanner_model"]="NA"
-  DATA["scanner_serial"]="NA"
-  DATA["scanner_firmware"]="NA"
 else
   usb_path=$( echo "$scanner_match" | grep -oP "libusb:[0-9]+:[0-9]+" )
 
@@ -58,24 +59,17 @@ else
 
   DATA["scanner_vendor"]=$( udevadm info --query=property --name="${dev_path}" | grep -m1 "ID_VENDOR=" | cut -d= -f2 || echo "NA" )
   DATA["scanner_model"]=$( udevadm info --query=property --name="${dev_path}" | grep -m1 "ID_MODEL=" | cut -d= -f2 || echo "NA" )
-  DATA["scanner_serial"]=$( udevadm info --query=property --name="${dev_path}" | grep -m1 "ID_SERIAL=" | cut -d= -f2 || echo "NA" )
-  DATA["scanner_firmware"]=$( udevadm info --query=property --name="${dev_path}" | grep -m1 "ID_REVISION=" | cut -d= -f2 || echo "NA" )
 fi
 
 # Printer
-#
-printer_dev=$( lsusb -v | awk '/^Bus/ {bus=$2; dev=$4} /bInterfaceClass +7 Printer/ { gsub(/:/,"",dev); print bus ":" dev; exit }')
+printer_dev=$( lsusb -v | awk '/^Bus/ {bus=$2; dev=$4} /bInterfaceClass +7 Printer/ { gsub(/:/,"",dev); print bus ":" dev; }') || printer_dev=""
 
 if [[ -z "$printer_dev" ]]; then
   DATA["printer_vendor"]="NA"
   DATA["printer_model"]="NA"
-  DATA["printer_serial"]="NA"
-  DATA["printer_firmware"]="NA"
 else
   DATA["printer_vendor"]=$( lsusb -v -s "${printer_dev}" | grep idVendor | xargs | cut -d" " -f2- || echo "NA")
   DATA["printer_model"]=$( lsusb -v -s "${printer_dev}" | grep idProduct | xargs | cut -d" " -f2- || echo "NA")
-  DATA["printer_serial"]=$( lsusb -v -s "${printer_dev}" | grep Serial | xargs | cut -d" " -f2- || echo "NA")
-  DATA["printer_firmware"]=$( lsusb -v -s "${printer_dev}" | grep idVendor | xargs | cut -d" " -f2- || echo "NA")
 fi
 
 # Text based log
@@ -101,6 +95,12 @@ for key in "${!DATA[@]}"; do
     printf '  "%s": "%s"' "$key" "${DATA[$key]}" >> "$JSON_FILE"
 done
 echo -e "\n}" >> "$JSON_FILE"
+
+# Change ownership and permissions to match votingworks logs
+chown syslog:adm $LOG_FILE
+chown syslog:adm $JSON_FILE
+chmod 640 $LOG_FILE
+chmod 640 $JSON_FILE
 
 echo "Files created:"
 echo "Log:  $LOG_FILE"
