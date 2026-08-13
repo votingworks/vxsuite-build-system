6c6075ac27903ee984d888abc565e9a4de71515f
arsalansufi <arsalan@voting.works>
2026-08-13 09:25:44 -0400

    Allow inventory selection for VxPollBook builds (#267)

--- scripts/prepare-vxpollbook-build.sh
+++ scripts/prepare-vxpollbook-build.sh
@@ -16,7 +16,7 @@ complete_system_dir="${local_user_home_dir}/code/vxsuite-complete-system"
 vxsuite_dir="${local_user_home_dir}/code/vxsuite"
 pollbook_dir="${vxsuite_dir}/apps/pollbook"
 
-ansible_inventory='vxpollbook-latest'
+ansible_inventory=$1
 
 if [[ ! -d ${vxsuite_build_system_dir}/inventories/${ansible_inventory} ]]; then
   echo "ERROR: The $ansible_inventory inventory could not be found."
